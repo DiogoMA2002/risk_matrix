78b512db47a38a8c75b9a081d8af793237e1d2bc
Diogo Andrade <psvdiogo@gmail.com>
2025-03-01 15:14:23 +0000

    SpringBoot starter

---
+++ risk_Matrix/src/test/java/ipleiria/risk_matrix/RiskMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package ipleiria.risk_matrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskMatrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
