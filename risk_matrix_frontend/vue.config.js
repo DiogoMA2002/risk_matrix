2944ee3b5474e4c0676f10451d4803d4f7e0bc00
Tiago Pereira <tpereira1911@gmail.com>
2025-07-12 14:08:20 +0100

    Public authentication

--- risk_matrix_frontend/vue.config.js
+++ risk_matrix_frontend/vue.config.js
@@ -10,14 +10,5 @@ module.exports = {
         }
       }
     }
-  },
-  devServer: {
-    port: 9090,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
   }
 };
